ab60a315f64c19a762ba9cb8b7c9ba25a3eb5cb8
Thomas Gale <gale.thomasjames@gmail.com>
2022-08-16 19:10:31 +0100

    Updated some padding on the fly.

--- src/side_bar.rs
+++ src/side_bar.rs
@@ -9,7 +9,7 @@ mod techs;
 #[function_component(SideBar)]
 pub fn side_bar() -> Html {
     html! {
-        <div class="basis-full sm:basis-1/3 md:basis-1/4 lg:basis-1/5 h-min sm:h-full p-2 bg-red-900">
+        <div class="basis-full sm:basis-1/3 md:basis-1/4 lg:basis-1/5 h-min sm:h-full p-4 bg-red-900">
             <name::Name/>
             <pic::Pic/>
             <bio::Bio/>
--- src/side_bar/pic.rs
+++ src/side_bar/pic.rs
@@ -3,6 +3,6 @@ use yew::prelude::*;
 #[function_component(Pic)]
 pub fn pic() -> Html {
     html! {
-        <img class="rounded-2xl pb-2" src={"/resources/duck_selfie.jpg"} alt={"Selfie of Thomas Gale with a duck"}/>
+        <img class="rounded-3xl pb-2" src={"/resources/duck_selfie.jpg"} alt={"Selfie of Thomas Gale with a duck"}/>
     }
 }
